4a8218632fefc3740689e48c1a2d68292a294af6
cola639 <337507950@qq.com>
2023-11-20 11:00:24 +0800

    add

---
+++ stack/stack.js
@@ -0,0 +1,40 @@
+function Stack() {
+  this._size = 0
+  this.values = {}
+}
+
+Stack.prototype.push = function (data) {
+  const size = ++this._size
+  this.values[size] = data
+}
+
+Stack.prototype.pop = function () {
+  const size = this._size
+  let deletedData = undefined
+
+  if (size) {
+    deletedData = this.values[size]
+
+    delete this.values[size]
+    this._size--
+
+    return deletedData
+  }
+}
+
+// 创建一个新的Stack对象
+let stack = new Stack()
+
+// 使用push方法添加元素
+stack.push("First")
+stack.push("Second")
+stack.push("Third")
+
+console.log(stack.values)
+// 现在，栈看起来应该是这样的：["First", "Second", "Third"]
+
+// 使用pop方法移除并获取栈顶元素
+let item = stack.pop() // item现在是"Third"
+
+console.log(stack.values)
+// 现在，栈看起来应该是这样的：["First", "Second"]
--- string/242.js
+++ string/242.js
@@ -15,3 +15,38 @@ var isAnagram = function (s, t) {
 const s = 'rat'
 const t = 'car'
 isAnagram(s, t)
+
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+s = "anagram", t = "nagaram"
+
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) {
+      return false; // 如果两个字符串长度不相等，它们不是异位词
+  }
+
+  const sObj = {};
+  const tObj = {};
+  
+  for (let item of s) {
+      sObj[item] = (sObj[item] || 0) + 1;
+  }
+
+  for (let item of t) {
+      tObj[item] = (tObj[item] || 0) + 1;
+  }
+
+  // 检查sObj和tObj是否相等
+  for (let key in sObj) {
+      if (sObj[key] !== tObj[key]) {
+          return false;
+      }
+  }
+
+  return true;
+};
+
+console.log(isAnagram("anagram", "nagaram")); // true
+console.log(isAnagram("rat", "car")); // false
